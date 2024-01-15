cd9813c1a1642218997c34c67491d6ef9384c0e2
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-15 03:49:18 -0800

    3.20*: Phonebook database, step8

--- models/person.js
+++ models/person.js
@@ -4,7 +4,16 @@ const Schema = mongoose.Schema;
 
 const PersonSchema = new Schema({
   name: { type: String, required: true, minLength: 3 },
-  number: { type: String, required: true }
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        // Regular expression for USA phone number format (111-111-1111)
+        return /^\d{3}-\d{3}-\d{4}$/.test(v);
+      }
+    }
+  }
 });
 
 // Export model
